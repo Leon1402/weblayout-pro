392c2d6327c87c747f64ecc94c30fe36649832d1
Никита Каплюхин <nikita-140294@mail.ru>
2021-05-14 16:32:48 +0700

    add new sections

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const swiper = new Swiper('.swiper__container', {
+const swiper = new Swiper('.hero__swiper', {
   allowTouchMove: false,
   loop: true,
   autoplay: {
-     delay: 6000,
+    delay: 6000,
   },
 });
 
@@ -23,6 +23,33 @@ const swiper2 = new Swiper(".galery-swiper", {
   },
 });
 
+const swiper3 = new Swiper(".editions__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  allowTouchMove: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper4 = new Swiper(".projects__swiper", {
+  slidesPerView: 3,
+  spaceBetween: 50,
+  setWrapperSize: '200px',
+  allowTouchMove: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const choices = new Choices('.galery__select', {});
+
 const headerButtons = document.querySelectorAll('.header-list__button');
 const dropdowns = document.querySelectorAll('.dropdown');
 
@@ -34,12 +61,12 @@ headerButtons.forEach(el => {
 
     dropdowns.forEach(item => {
       if (item != drop)
-      item.classList.remove('dropdown_active');
+        item.classList.remove('dropdown_active');
     })
 
     headerButtons.forEach(item => {
       if (item != e.currentTarget)
-      item.classList.remove('header-list__button_active');
+        item.classList.remove('header-list__button_active');
     })
 
     drop.classList.toggle('dropdown_active');
@@ -47,7 +74,7 @@ headerButtons.forEach(el => {
 })
 
 dropdowns.forEach(el => {
-  el.addEventListener('click', e => {e._isDropdownClick = true})
+  el.addEventListener('click', e => { e._isDropdownClick = true })
 });
 
 window.addEventListener('click', e => {
@@ -61,3 +88,39 @@ window.addEventListener('click', e => {
   }
 });
 
+tippy('.tooltip1', {
+  content: 'Пример современных тенденций - современная методология разработки',
+  maxWidth: 264,
+});
+
+tippy('.tooltip2', {
+  content: 'Приятно, граждане, наблюдать, как сделанные на базе аналитики выводы вызывают у вас эмоции  ',
+  maxWidth: 264,
+});
+
+tippy('.tooltip3', {
+  content: 'В стремлении повысить качество ',
+  maxWidth: 264,
+});
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map('mymap', {
+    center: [48.872197, 2.354224],
+    zoom: 13,
+    controls: []
+  });
+
+  // myPlacemark = new ymaps.Placemark([48.872197, 2.354224], {
+  //   hintContent: 'улица дю Фобур Сен Дени, 54',
+  // }, {
+  //   iconLayout: 'default#image',
+  //   iconImageHref: 'src/icon.svg',
+  //   iconImageSize: [28, 40],
+  //   iconImageOffset: [-5, -38]
+  // }),
+
+  //   myMap.geoObjects
+  //     .add(myPlacemark)
+}
